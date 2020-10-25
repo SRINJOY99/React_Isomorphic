c327a6f921dbb13394e367f95d9acd10a3131883
SRINJOY99 <srinjoysingh99@gmail.com>
2020-10-25 22:17:49 +0530

    Final Code

---
+++ build/precache-manifest.4e5366029dc4db91da7d31fc203186cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de48dc0998251822153aa86b593fd83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3888242f14270302c6a0",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "9b6eb93ebdb31b4f9c94",
+    "url": "/static/css/main.ba1b50d0.chunk.css"
+  },
+  {
+    "revision": "3888242f14270302c6a0",
+    "url": "/static/js/2.5f635476.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5f635476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b6eb93ebdb31b4f9c94",
+    "url": "/static/js/main.5fe509ef.chunk.js"
+  },
+  {
+    "revision": "0980db0307ffad65803b",
+    "url": "/static/js/runtime-main.df5858f9.js"
+  }
+]);
